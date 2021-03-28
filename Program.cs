d0bfeb3a015be4ca9a8be35ca33e4f229621348c
francisgail <francisgail.pena@yahoo.com>
2021-03-28 19:55:45 -0400

    disable mouse input console

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Timer = System.Timers.Timer;
 
@@ -7,12 +8,44 @@ namespace LiveStatsPoller
 {
     class Program
     {
+
+        const uint ENABLE_QUICK_EDIT = 0x0040;
+
+        // STD_INPUT_HANDLE (DWORD): -10 is the standard input device.
+        const int STD_INPUT_HANDLE = -10;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
         private static int _pollCount;
 
         private static Timer _timer;
 
         static void Main(string[] args)
         {
+            IntPtr consoleHandle = GetStdHandle(STD_INPUT_HANDLE);
+
+            // get current console mode
+            uint consoleMode;
+            if (!GetConsoleMode(consoleHandle, out consoleMode))
+            {
+                // ERROR: Unable to get console mode.
+               
+            }
+
+            // Clear the quick edit bit in the mode flags
+            consoleMode &= ~ENABLE_QUICK_EDIT;
+            // set the new mode
+            if (!SetConsoleMode(consoleHandle, consoleMode))
+            {
+                // ERROR: Unable to set console mode
+                //return false;
+            }
             Console.CursorVisible = false;
             Console.Clear();
             Console.WriteLine("Starting to poll....");
@@ -26,11 +59,10 @@ namespace LiveStatsPoller
 
             _timer.Elapsed += timer_Elapsed;
             _timer.Enabled = true;
-            
+            //DisableQuickEdit();
             while (true)
             {
-                Console.ReadKey();
-
+                //Console.ReadKey();
             };
         }
 
@@ -41,17 +73,24 @@ namespace LiveStatsPoller
             Console.CursorTop = 1;
             Console.Write($"{utcTimeNow}");
 
-            if (utcTimeNow.Minute % 5 == 0)
+            if (utcTimeNow.Second == 0 && utcTimeNow.Minute % 5 == 0)
             {
-                _pollCount++;
+                
                 Task.Run(async () =>
                 {
                     var pollResult = await Fetch();
                     Console.CursorLeft = 0;
                     Console.CursorTop = 2;
                     Console.Write($"Last Poll result: {pollResult}");
+                    
+                    var rankTeamsResult = await RankTeams();
                     Console.CursorLeft = 0;
                     Console.CursorTop = 3;
+                    Console.Write($"RankTeam result: {rankTeamsResult}");
+                    
+                    _pollCount++;
+                    Console.CursorLeft = 0;
+                    Console.CursorTop = 4;
                     Console.Write($"Polls made: {_pollCount}");
                     if (pollResult == "Games Done")
                     {
@@ -72,5 +111,19 @@ namespace LiveStatsPoller
 
             return result;
         }
+
+        private static async Task<string> RankTeams()
+        {
+            var httpService = new HttpService();
+
+            var result = await httpService.GetAsync<string>("https://api.timkoto.com/dev/api/utility/v1/RankTeams", new Dictionary<string, string>
+            {
+                {"x-api-key", "052d7c2822msh1effd682c0dbce0p113fabjsn219fbe03967c"}
+            });
+
+            return result;
+        }
+
+       
     }
 }
